eb01ad2ce249fe691951868f888c864590848465
friedsunfish <friedsunfish@gmail.com>
2023-01-21 18:50:11 +0900

    back_1

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+new Swiper(".swiper", {
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  slidesPerView: 1,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
